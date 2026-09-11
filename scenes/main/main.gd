4125a3b9729f06a1a0abdf56fb0452e4ab8f3ff4
yfby <harveykyleybanez@gmail.com>
2026-09-12 01:29:04 +0800

    feat: test

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,11 +1,7 @@
-extends Node2D
+extends Node
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_classroom()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func load_classroom() -> void:
+	SceneManager.change_scene("res://scenes/levels/classroom.tscn")
